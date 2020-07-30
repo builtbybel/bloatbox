b65e7e438bde82004fa16e00d28bd0ad88052c11
Belim <github@mirinsoft.com>
2020-07-30 19:00:43 +0200

    Add files via upload

---
+++ marketplace/Reinstall all built-in apps.ps1
@@ -0,0 +1,4 @@
+### This scipt will reinstall all built-in modern apps in a single step ###
+###- This command will take a few moments and will reinstall all built-in apps. If you receive any error message, ignore it. You may need to restart the computer to take effect.###
+###- Bloatbox must be executed as administrator.
+Get-AppxPackage -allusers | foreach {Add-AppxPackage -register "$($_.InstallLocation)\appxmanifest.xml" -DisableDevelopmentMode}
